c1abc4eb789d8cf3e2b1936278e344c32dd812be
Urosh <urosh_h@protonmail.com>
2023-08-29 14:45:30 +0200

    module & config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@ehc/api/database';
 import { UsersModule } from '@ehc/api/users';
+import { CategoriesModule } from '@ehc/api/categories';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
