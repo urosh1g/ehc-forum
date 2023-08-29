1d6865a079f92dc5243075c9d3c8053d49aa19ec
Urosh <urosh_h@protonmail.com>
2023-08-29 21:34:15 +0200

    initial post controller impl

--- libs/api/posts/src/lib/posts.controller.ts
+++ libs/api/posts/src/lib/posts.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post as HttpPost, Body, Patch } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { Post } from '@ehc/common/entities';
+import { CreatePost, UpdatePost } from '@ehc/common/dtos'
 
 @Controller('posts')
 export class PostsController {
-  constructor(private PostsService: PostsService) {}
+  constructor(private postsService: PostsService) {}
+
+  @Get('')
+  fetchAll(): Promise<Post[]> {
+    return this.postsService.fetchAll();
+  }
+
+  @Get(':id')
+  fetchOne(@Param('id', ParseIntPipe)id: number): Promise<Post> {
+    return this.postsService.fetchOne(id);
+  }
+
+  @HttpPost()
+  create(@Body()dto: CreatePost): Promise<Post> {
+    return this.postsService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe)id: number, @Body() dto: UpdatePost): Promise<Post> {
+    return this.postsService.update(id, dto);
+  }
 }
--- libs/api/posts/src/lib/posts.service.ts
+++ libs/api/posts/src/lib/posts.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { CreatePost, UpdatePost } from '@ehc/common/dtos';
+import { Post } from '@ehc/common/entities';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(
+        @InjectRepository(Post)
+        private postRepository: Repository<Post>
+    ) {}
+    fetchAll(): Promise<Post[]> {
+        return this.postRepository.find();
+    }
+    async fetchOne(id: number): Promise<Post> {
+        const post = await this.postRepository.findOneBy({id});
+        if(!post) {
+            throw new NotFoundException(`Post with id ${id} was not found`);
+        }
+        return post
+    }
+
+    async create(dto: CreatePost): Promise<Post> {
+        const post = this.postRepository.create(dto);
+        return await this.postRepository.save(post);
+    }
+    async update(id: number, dto: UpdatePost): Promise<Post> {
+        const post = await this.postRepository.findOneBy({id});
+        if(!post) {
+            throw new NotFoundException(`Post with id ${id} was not found`);
+        }
+        await this.postRepository.update(id, dto);
+        return (await this.postRepository.findOneBy({id}))!;
+    }
+
+    async delete(id: number): Promise<Post> {
+        const post = await this.postRepository.findOneBy({id});
+        if(!post) {
+            throw new NotFoundException(`Post with id ${id} was not found`);
+        }
+        this.postRepository.delete(id);
+        return post;
+    }
+}
