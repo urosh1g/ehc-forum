cfe097068e94dd72c7d5f50484984946a1f2ca55
Urosh <urosh_h@protonmail.com>
2023-08-29 14:12:02 +0200

    initial packages & minor changes

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@ehc/api/database';
+import { UsersModule } from '@ehc/api/users';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
